eab0b34b088aec293bcec77c11282eca414df535
Leo Fontes <leofontes@gmail.com>
2017-01-26 15:13:06 -0200

    Miracle pill implementation

--- MiraclePills/MiraclePills/ViewController.swift
+++ MiraclePills/MiraclePills/ViewController.swift
@@ -8,11 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerBtn: UIButton!
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var countryTxtField: UITextField!
+    @IBOutlet weak var buyBtn: UIButton!
+    
+    let states = ["Alaska", "Arkansas", "Maine", "California", "New York"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        statePicker.dataSource = self
+        statePicker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +31,34 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func stateBtnPressed(_ sender: Any) {
+        statePicker.isHidden = false
+        countryLabel.isHidden = true
+        countryTxtField.isHidden = true
+        buyBtn.isHidden = true
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerBtn.setTitle(states[row], for: UIControlState.normal)
+        countryLabel.isHidden = false
+        countryTxtField.isHidden = false
+        buyBtn.isHidden = false
+        statePicker.isHidden = true
+    }
+    @IBAction func buyBtnPressed(_ sender: Any) {
+        
+    }
 }
 

10f26a221a0ae472a380b5fbb3730558b8377c5e
Leo Fontes <leofontes@gmail.com>
2017-02-09 15:41:12 -0200

    Adapter

---
+++ DreamLister/DreamLister/ItemCell.swift
@@ -0,0 +1,17 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Leo Fontes on 09/02/17.
+//  Copyright © 2017 Leo Fontes. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+}

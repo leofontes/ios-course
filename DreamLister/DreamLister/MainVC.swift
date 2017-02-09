a14b1d238407a0c15af57a584470c46e8cf269eb
Leo Fontes <leofontes@gmail.com>
2017-02-09 14:58:45 -0200

    CustomView, layout

--- DreamLister/DreamLister/MainVC.swift
+++ DreamLister/DreamLister/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  DreamLister
 //
 //  Created by Leo Fontes on 09/02/17.
@@ -8,18 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 

e1ccad77639abdaa881087b217ca15b3a07b6c27
Leo Fontes <leofontes@gmail.com>
2017-02-16 16:48:22 -0200

    Initial commit RainyShiny weather app

---
+++ RainyShiny/RainyShiny/WeatherVC.swift
@@ -0,0 +1,41 @@
+//
+//  WeatherVC.swift
+//  RainyShiny
+//
+//  Created by Leo Fontes on 16/02/17.
+//  Copyright © 2017 Leo Fontes. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var forecastImage: UIImageView!
+    @IBOutlet weak var forecastLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+        return cell
+    }
+
+}
+

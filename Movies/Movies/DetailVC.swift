a2ed7612d7680ee9d51942111d986840594e2da4
Leo Fontes <leofontes@gmail.com>
2017-02-17 21:12:01 -0200

    Basic layout of home and organization of files

---
+++ Movies/Movies/DetailVC.swift
@@ -0,0 +1,16 @@
+//
+//  DetailVC.swift
+//  Movies
+//
+//  Created by Leonardo Thives da Luz Fontes on 2/17/17.
+//  Copyright © 2017 Leo Fontes. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Movies/Movies/HomeVC.swift
@@ -0,0 +1,21 @@
+//
+//  HomeVC.swift
+//  Movies
+//
+//  Created by Leonardo Thives da Luz Fontes on 2/17/17.
+//  Copyright © 2017 Leo Fontes. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+    @IBOutlet var segmentedControl: UISegmentedControl!
+    @IBOutlet var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+

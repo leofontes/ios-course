481d80f22fa1a987ce435bcb6cf9f5d41c40606b
Leo Fontes <leofontes@gmail.com>
2017-03-02 14:15:17 -0300

    PitchPerfect initial commit

---
+++ PitchPerfect/PitchPerfect/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Leo Fontes on 02/03/17.
+//  Copyright © 2017 Leo Fontes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func recordAudio(_ sender: Any) {
+        
+    }
+}
+
